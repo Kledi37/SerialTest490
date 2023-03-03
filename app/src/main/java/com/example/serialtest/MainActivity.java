4b092b99fa240d82b891a0108480181a9ba8a15d
Kledi37 <kledi37@gmail.com>
2023-03-02 21:25:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/serialtest/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.serialtest;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
+
+import android.hardware.usb.UsbDevice;
+import android.os.Handler;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import me.aflak.arduino.Arduino;
+import me.aflak.arduino.ArduinoListener;
+public class MainActivity extends AppCompatActivity implements ArduinoListener {
+    private Arduino arduino;
+    private TextView displayTextView;
+    private EditText editText;
+    private Button sendBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        displayTextView = findViewById(R.id.diplayTextView);
+        editText = findViewById(R.id.editText);
+        sendBtn = findViewById(R.id.sendBtn);
+        displayTextView.setMovementMethod(new    ScrollingMovementMethod());
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String editTextString =   editText.getText().toString();
+                arduino.send(editTextString.getBytes());
+                editText.getText().clear();
+            }
+        });
+        arduino = new Arduino(this);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        arduino.setArduinoListener(this);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        arduino.unsetArduinoListener();
+        arduino.close();
+    }
+    @Override
+    public void onArduinoAttached(UsbDevice device) {
+        display("arduino attached...");
+        arduino.open(device);
+    }
+    @Override
+    public void onArduinoDetached() {
+        display("arduino detached.");
+    }
+    @Override
+    public void onArduinoMessage(byte[] bytes) {
+        display(new String(bytes));
+    }
+    @Override
+    public void onArduinoOpened() {
+        String str = "arduino opened...";
+        arduino.send(str.getBytes());
+    }
+    @Override
+    public void onUsbPermissionDenied() {
+        display("Permission denied. Attempting again in 3 sec...");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                arduino.reopen();
+            }
+        }, 3000);
+    }
+    private void display(final String message){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                displayTextView.append(message);
+            }
+        });
+    }
+}
